a54647dba1b2e7469810072ee6230ee57a20f65d
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-04-15 12:45:16 -0700

    Implemented shell REPL

---
+++ shell/shell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LINE_MAX 1024
+#define PROMPT   "sish:> "
+
+// Returns the exit status, e.g. 0 on normal exit
+void run()
+{
+  while (1) {
+    char line[LINE_MAX + 1]; // Account for null-terminator
+    printf("%s", PROMPT);
+
+    if (fgets(line, LINE_MAX + 1, stdin) == NULL
+        || strcmp(line, "exit\n") == 0) {
+      return;
+    } else {
+      printf("You entered: %s", line);
+    }
+
+    printf("\n");
+  }
+}
+
+int main(int argc, char **argv)
+{
+  run();
+  return 0;
+}
+
